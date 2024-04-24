1d68412d4550cce79f6dfd54d4bd33ef8f855810
김신영 <estumav@gmail.com>
2024-04-24 14:36:58 +0900

    ✨ feat: 회원관리 회원 정보 조회

--- backend/user/src/main/java/com/luckyseven/user/user/controller/UserController.java
+++ backend/user/src/main/java/com/luckyseven/user/user/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 @Tag(name = "User", description = "회원 API")
@@ -28,7 +29,7 @@ public class UserController {
             @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자"),
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
-    public ResponseEntity<MyInfoDto> getMyInfo(@Parameter(hidden = true) @RequestHeader("loggedInUser") String userId) {
+    public ResponseEntity<MyInfoDto> getMyInfo(@RequestHeader("loggedInUser") String userId) {
 
         MyInfoDto myInfo = userService.getMyInfo(userId);
 
@@ -40,14 +41,14 @@ public class UserController {
         return ResponseEntity.status(200).body(myInfo);
     }
 
-    @GetMapping("/me")
+    @DeleteMapping("/me")
     @Operation(summary = "회원 탈퇴", description = "회원 탈퇴")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자"),
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
-    public ResponseEntity<?> deleteMyId(@Parameter(hidden = true) @RequestHeader("loggedInUser") String userId) {
+    public ResponseEntity<?> deleteMyId(@RequestHeader("loggedInUser") String userId) {
         // TODO : SINYEONG
         userService.deleteUser(userId);
 
