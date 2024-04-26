5f997f2c7203ee36f4c1503b22460f4869a7b131
김신영 <estumav@gmail.com>
2024-04-26 16:19:17 +0900

    ✨ feat: joinEvent 추가

--- backend/event/src/main/java/com/luckyseven/event/rollsheet/repository/RollSheetRepository.java
+++ backend/event/src/main/java/com/luckyseven/event/rollsheet/repository/RollSheetRepository.java
@@ -2,6 +2,7 @@ package com.luckyseven.event.rollsheet.repository;
 
 import com.luckyseven.event.rollsheet.entity.RollSheet;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
@@ -11,5 +12,8 @@ public interface RollSheetRepository extends MongoRepository<RollSheet, String> 
     List<RollSheet> findByEventId(int eventId);
     int countByEventId(int eventId);
     void deleteAllByEventId(int eventId);
+
+    @Query(value = "{ 'userId' : ?0 }", fields = "{ 'eventId' : 1 }")
+    List<RollSheet> findEventIdsByUserId(String userId);
 
 }
